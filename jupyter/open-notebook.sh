68f647e49ff2960dd013aabb5ef9d848cebfa101
avouacr <avouacr@pm.me>
2024-09-24 09:10:35 +0000

    fix permissions

--- jupyter/open-notebook.sh
+++ jupyter/open-notebook.sh
@@ -4,8 +4,10 @@
 # named `tutorial.ipynb` and opens it in Jupyter Lab at startup
 # Expected parameters : None
 
-# Clone Git repository
-git clone --depth 1 https://github.com/InseeFrLab/sspcloud-init-scripts.git
+# Clone repository and give permissions to the onyxia user
+GIT_REPO=sspcloud-init-scripts
+git clone --depth 1 https://github.com/InseeFrLab/${GIT_REPO}.git
+chown -R onyxia:users ${GIT_REPO}/
 
 # Open the relevant notebook when starting Jupyter Lab
-echo "c.LabApp.default_url = '/lab/tree/sspcloud-init-scripts/tutorial.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
+echo "c.LabApp.default_url = '/lab/tree/${GIT_REPO}/tutorial.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
--- rstudio/open-rproj.sh
+++ rstudio/open-rproj.sh
@@ -8,7 +8,7 @@ REPO_URL=https://github.com/avouacr/learnr-onyxia.git
 WORK_DIR=/home/onyxia/work
 TUTO_DIR=${WORK_DIR}/tutoriel
 
-# Clone the repository and give permissions to the onyxia user 
+# Clone repository and give permissions to the onyxia user 
 git clone $REPO_URL $TUTO_DIR
 chown -R onyxia:users $TUTO_DIR
 
