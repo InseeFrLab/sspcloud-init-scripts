a2831763bbfc7d7c8334d06a08efda475ad68464
avouacr <avouacr@pm.me>
2024-09-25 11:38:03 +0000

    init script to install R packages

---
+++ rstudio/install-packages.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# This init script presents various ways of installing R packages
+# Expected parameters : None
+
+# Clone repository and give permissions to the onyxia user
+GIT_REPO=sspcloud-init-scripts
+git clone --depth 1 https://github.com/InseeFrLab/${GIT_REPO}.git
+chown -R onyxia:users ${GIT_REPO}/
+
+# Install the remote package (needed for remotes install)
+Rscript -e "install.packages('remotes')"
+
+# Install from a `DESCRIPTION` file that specifies project dependencies
+# See https://r-pkgs.org/description.html for more info
+Rscript -e "remotes::install_deps('examples')"
+
+# Install from a `renv` lockfile
+# See https://rstudio.github.io/renv/articles/renv.html for more info
+Rscript -e "install.packages('renv')"
+Rscript -e "renv::restore('examples')"
+
+# Install from 
+# See https://remotes.r-lib.org/reference/install_github.html for more info
+# See https://remotes.r-lib.org/reference/install_gitlab.html for the equivalent GitLab function
+Rscript -e "remotes::install_github('tidyverse/ggplot2')"
