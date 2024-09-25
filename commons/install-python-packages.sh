b15ab2ad23d4561a0688e25b88b8c4b3f78923a2
avouacr <avouacr@pm.me>
2024-09-25 12:59:39 +0000

    various vays of installing py package

--- commons/install-python-packages.sh
+++ commons/install-python-packages.sh
@@ -9,5 +9,11 @@ GIT_REPO=sspcloud-init-scripts
 git clone --depth 1 https://github.com/InseeFrLab/${GIT_REPO}.git
 chown -R onyxia:users ${GIT_REPO}/
 
-# Install Python packages
+# Install Python packages using `pip`
+pip install matplotlib plotly
+
+# Install Python packages using `pip` and a `requirements.txt` file
 pip install -r ${GIT_REPO}/inputs/requirements.txt
+
+# Install Python packages using `conda`
+conda install requests scipy
