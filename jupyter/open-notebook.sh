377d170ee9b3a6429899eb8482f47a5dda84358f
avouacr <avouacr@pm.me>
2024-09-25 09:25:27 +0000

    improve

--- jupyter/open-notebook.sh
+++ jupyter/open-notebook.sh
@@ -9,5 +9,9 @@ GIT_REPO=sspcloud-init-scripts
 git clone --depth 1 https://github.com/InseeFrLab/${GIT_REPO}.git
 chown -R onyxia:users ${GIT_REPO}/
 
+# Install additional packages if a requirements.txt file is present in the project
+REQUIREMENTS_FILE=${GIT_REPO}/requirements.txt
+[ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
+
 # Open the relevant notebook when starting Jupyter Lab
 echo "c.LabApp.default_url = '/lab/tree/${GIT_REPO}/examples/tutorial.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
