2be8a04881e8cd696d32e4066eb25e458764e2af
ihiverlet <ines.hiverlet@insee.fr>
2024-10-16 09:33:26 +0000

    split open-rproj-custom.sh according the use cases

---
+++ rstudio/customize-settings.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# This init script updates settings of Rsudio. Various examples of options are displayed.
+# Expected parameters : None
+
+# Set a theme and displays the console on the right
+echo \
+"
+setHook('rstudio.sessionInit', function(newSession) {
+  if (newSession && identical(getwd(), '${WORKSPACE_DIR}'))
+  {
+    # For a slick dark theme (⌐■_■)
+    rstudioapi::applyTheme('Merbivore')
+    # Console on the right
+    rstudioapi::executeCommand('layoutConsoleOnRight')
+  }
+}, action = 'append')
+
+# Set CRAN mirror
+options(repos = c(CRAN = 'https://cran.rstudio.org'))
+
+# Customize console prompt, digit display, and continuation prompt
+options(
+  prompt = 'R> ',      # Custom prompt
+  digits = 4,          # Limit numeric precision to 4 digits
+  continue = '+++ '    # Continuation prompt
+)
+" >> /home/onyxia/.Rprofile
